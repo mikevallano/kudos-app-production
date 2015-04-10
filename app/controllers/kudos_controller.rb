69006340251074215134cc1331480b4f72961566
mike vallano <mikevallano@gmail.com>
2015-04-10 10:02:00 -0500

    updated some more views and restrictions for non-admins

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -1,6 +1,6 @@
 class KudosController < ApplicationController
   before_action :set_kudo, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
 
   # GET /kudos
   # GET /kudos.json
