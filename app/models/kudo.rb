9c6b7a739d272b7f0d79536b0234984618652287
mike vallano <mikevallano@gmail.com>
2015-04-20 10:15:08 -0500

    added model validation

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -3,4 +3,6 @@ class Kudo < ActiveRecord::Base
   belongs_to :receiver, class_name: 'User'
   has_many :kudoizations
   has_many :kudotypes, through: :kudoizations
+
+  validates :comments, presence: true
 end
--- app/models/kudotype.rb
+++ app/models/kudotype.rb
@@ -1,4 +1,6 @@
 class Kudotype < ActiveRecord::Base
   has_many :kudoizations
   has_many :kudos, through: :kudoizations
+
+  validates :name, presence: true
 end
