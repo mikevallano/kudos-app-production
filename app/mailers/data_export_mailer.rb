29fd0c8a5f27fa3d6df0ee9fb52afecdb1cc034f
mike vallano <mikevallano@gmail.com>
2016-11-06 14:59:41 -0600

    Update Rake Task

--- app/mailers/data_export_mailer.rb
+++ app/mailers/data_export_mailer.rb
@@ -2,7 +2,7 @@ class DataExportMailer < ApplicationMailer
 
   def send_data_export
     attachments['kudos_report.csv'] = File.read('tmp/kudos_report.csv')
-    mail( to: 'mikevallano@gmail.com', from: 'test@tester.com', :subject => "Report" )
+    mail( to: 'test@tester.com', from: 'test@tester.com', :subject => "Report" )
   end
 
 end
